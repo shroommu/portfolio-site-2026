88a8c07ee295a8d7b4eef64597e01418757bace3
shroommu <alexa.kruckenberg@gmail.com>
2026-07-26 12:02:46 -0600

    add attribution

--- components/AppFooter.tsx
+++ components/AppFooter.tsx
@@ -1,3 +1,7 @@
 export default function AppFooter() {
-  return <footer className="p-8"></footer>;
+  return (
+    <footer className="p-4 text-center bg-[var(--background)]">
+      <p>© {new Date().getFullYear()} Alex Kruckenberg</p>
+    </footer>
+  );
 }
